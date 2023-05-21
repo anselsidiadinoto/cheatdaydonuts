ccfaf704bd0789fad973a9c4f8a280d5d6dd41bf
Ansel Sidiadinoto <anselsidiadinoto@gmail.com>
2023-05-21 12:21:20 -0400

    /

--- src/menu_item/adminRoutes.js
+++ src/menu_item/adminRoutes.js
@@ -15,11 +15,10 @@ const pgSessionConnectionObj = {
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
+  port: process.env.DB_PORT,
 };
 
-const conString = process.env.CONNECTION_STRING;
-
+// const conString = process.env.CONNECTION_STRING;
 // const pgSessionConnectionObj = conString;
 
 const pgSession = require('connect-pg-simple')(session);
